fe5a04ee02a960af7ee5792c16f4b5f7be32e953
Peter Tountas <petertountas1@gmail.com>
2023-11-26 12:45:10 -0600

    updates

---
+++ README.md
@@ -0,0 +1,28 @@
+#### first download the pre-trained weights
+
+```
+cog run script/download-weights
+```
+
+#### then you can run predictions
+
+```
+cog predict -i input_images=@zeke.zip -i use_face_detection_instead=True
+```
+
+### Example
+
+---
+
+Output is a `trained_model.tar` file
+
+#### Civit location of coloring book weights:
+
+```
+https://civitai.com/models/136348/coloringbookredmond-coloring-book-lora-for-sd-xl
+
+```
+
+#### base on this repo on replicate:
+
+[realvisxl2-lora-training](https://replicate.com/lucataco/realvisxl2-lora-training?input=form&output=preview)
---
+++ script/download_weights.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Run this before you deploy it on replicate
+import os
+import sys
+import torch
+from diffusers import DiffusionPipeline, AutoencoderKL
+
+# append project directory to path so predict.py can be imported
+sys.path.append('.')
+from predict import MODEL_NAME, MODEL_CACHE, COLORING_BOOK_MODEL_NAME, COLORING_BOOK_WEIGHTS_NAME
+
+# Make cache folders
+if not os.path.exists(MODEL_CACHE):
+    os.makedirs(MODEL_CACHE)
+
+# Download SDXL-VAE-FP16-Fix
+better_vae = AutoencoderKL.from_pretrained(
+    "madebyollin/sdxl-vae-fp16-fix",
+    torch_dtype=torch.float16,
+)
+
+# Download RealvisXl-v2.0
+pipe = DiffusionPipeline.from_pretrained(
+    MODEL_NAME,
+    vae=better_vae,
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+)
+
+pipe.load_lora_weights(COLORING_BOOK_MODEL_NAME, weight_name=COLORING_BOOK_WEIGHTS_NAME, adapter_name="coloringbook")
+pipe.set_adapters("coloringbook", adapter_weights=1.0, adapter_names=["coloringbook"])
+
+pipe.save_pretrained(MODEL_CACHE, safe_serialization=True).to("cuda")
