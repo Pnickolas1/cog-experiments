a3092b9807ab09e7b42035f42b0a176a77d0a578
Peter Tountas <petertountas1@gmail.com>
2023-12-02 20:32:29 -0600

    udates

--- script/download_weights.py
+++ script/download_weights.py
@@ -1,39 +1,73 @@
-#!/usr/bin/env python
 
-# Run this before you deploy it on replicate
-import os
-import sys
+
+# Run this before you deploy it on replicate, because if you don't
+# whenever you run the model, it will download the weights from the
+# internet, which will take a long time.
+
 import torch
-from diffusers import DiffusionPipeline, AutoencoderKL
+from diffusers import AutoencoderKL, DiffusionPipeline
+from diffusers.pipelines.stable_diffusion.safety_checker import (
+    StableDiffusionSafetyChecker,
+)
 import peft
-
-# append project directory to path so predict.py can be imported
-sys.path.append('.')
 from predict import MODEL_NAME, MODEL_CACHE, COLORING_BOOK_MODEL_NAME, COLORING_BOOK_WEIGHTS_NAME
 
-# Make cache folders
-if not os.path.exists(MODEL_CACHE):
-    os.makedirs(MODEL_CACHE)
 
-# Download SDXL-VAE-FP16-Fix
+# # Download RealvisXl-v2.0
+# pipe = DiffusionPipeline.from_pretrained(
+#     MODEL_NAME,
+#     vae=better_vae,
+#     torch_dtype=torch.float16,
+#     use_safetensors=True,
+# )
+
+# pipe = DiffusionPipeline.from_pretrained(
+#     "stabilityai/stable-diffusion-xl-base-1.0",
+#     torch_dtype=torch.float16,
+#     use_safetensors=True,
+#     variant="fp16",
+# )
+
+# pipe.save_pretrained("./cache", safe_serialization=True)
+
 better_vae = AutoencoderKL.from_pretrained(
-    "madebyollin/sdxl-vae-fp16-fix",
-    torch_dtype=torch.float16,
+    "madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16
 )
 
-# Download RealvisXl-v2.0
 pipe = DiffusionPipeline.from_pretrained(
-    MODEL_NAME,
+    "stabilityai/stable-diffusion-xl-base-1.0",
     vae=better_vae,
     torch_dtype=torch.float16,
     use_safetensors=True,
+    variant="fp16",
 )
 
-pipe.load_lora_weights(COLORING_BOOK_MODEL_NAME, weight_name=COLORING_BOOK_WEIGHTS_NAME, adapter_name="coloringbook")
-pipe.set_adapters("coloringbook")
-#pipe.set_adapters("coloringbook", adapter_weights=1.0, adapter_names=["coloringbook"])  --> did not work
-#pipe.set_adapters(adapter_weights=1.0, adapter_names=["coloringbook"]) 
-# First, fuse the LoRA parameters.
-pipe.fuse_lora()
+pipe.save_pretrained("./sdxl-cache", safe_serialization=True)
+
+pipe = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-refiner-1.0",
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+    variant="fp16",
+)
+
+# TODO - we don't need to save all of this and in fact should save just the unet, tokenizer, and config.
+pipe.save_pretrained("./refiner-cache", safe_serialization=True)
+
+
+safety = StableDiffusionSafetyChecker.from_pretrained(
+    "CompVis/stable-diffusion-safety-checker",
+    torch_dtype=torch.float16,
+)
+
+# pipe.load_lora_weights(COLORING_BOOK_MODEL_NAME, weight_name=COLORING_BOOK_WEIGHTS_NAME, adapter_name="coloringbook")
+# pipe.set_adapters("coloringbook")
+# #pipe.set_adapters("coloringbook", adapter_weights=1.0, adapter_names=["coloringbook"])  --> did not work
+# #pipe.set_adapters(adapter_weights=1.0, adapter_names=["coloringbook"]) 
+# # First, fuse the LoRA parameters.
+# pipe.fuse_lora()
 
 pipe.save_pretrained(MODEL_CACHE, safe_serialization=True)
+
+
+safety.save_pretrained("./safety-cache")
