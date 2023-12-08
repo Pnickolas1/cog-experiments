acd1bb8fb5740435480b1934d520be85343e5afc
Peter Tountas <petertountas1@gmail.com>
2023-12-07 20:40:08 -0600

    testing

--- predict.py
+++ predict.py
@@ -76,92 +76,92 @@ def download_weights(url, dest, extract=True):
     print("downloading took: ", time.time() - start)
 
 class Predictor(BasePredictor):
-    def load_trained_weights(self, weights, pipe):
-        from no_init import no_init_or_tensor
+    # def load_trained_weights(self, weights, pipe):
+    #     from no_init import no_init_or_tensor
 
-        # weights can be a URLPath, which behaves in unexpected ways
-        weights = str(weights)
-        if self.tuned_weights == weights:
-            print("skipping loading .. weights already loaded")
-            return
+    #     # weights can be a URLPath, which behaves in unexpected ways
+    #     weights = str(weights)
+    #     if self.tuned_weights == weights:
+    #         print("skipping loading .. weights already loaded")
+    #         return
 
-        self.tuned_weights = weights
+    #     self.tuned_weights = weights
 
-        local_weights_cache = self.weights_cache.ensure(weights)
+    #     local_weights_cache = self.weights_cache.ensure(weights)
 
-        # load UNET
-        print("Loading fine-tuned model")
-        self.is_lora = False
+    #     # load UNET
+    #     print("Loading fine-tuned model")
+    #     self.is_lora = False
 
-        maybe_unet_path = os.path.join(local_weights_cache, "unet.safetensors")
-        if not os.path.exists(maybe_unet_path):
-            print("Does not have Unet. assume we are using LoRA")
-            self.is_lora = True
+    #     maybe_unet_path = os.path.join(local_weights_cache, "unet.safetensors")
+    #     if not os.path.exists(maybe_unet_path):
+    #         print("Does not have Unet. assume we are using LoRA")
+    #         self.is_lora = True
 
-        if not self.is_lora:
-            print("Loading Unet")
+    #     if not self.is_lora:
+    #         print("Loading Unet")
 
-            new_unet_params = load_file(
-                os.path.join(local_weights_cache, "unet.safetensors")
-            )
-            # this should return _IncompatibleKeys(missing_keys=[...], unexpected_keys=[])
-            pipe.unet.load_state_dict(new_unet_params, strict=False)
+    #         new_unet_params = load_file(
+    #             os.path.join(local_weights_cache, "unet.safetensors")
+    #         )
+    #         # this should return _IncompatibleKeys(missing_keys=[...], unexpected_keys=[])
+    #         pipe.unet.load_state_dict(new_unet_params, strict=False)
 
-        else:
-            print("Loading Unet LoRA")
+    #     else:
+    #         print("Loading Unet LoRA")
 
-            unet = pipe.unet
+    #         unet = pipe.unet
 
-            tensors = load_file(os.path.join(local_weights_cache, "lora.safetensors"))
+    #         tensors = load_file(os.path.join(local_weights_cache, "lora.safetensors"))
 
-            unet_lora_attn_procs = {}
-            name_rank_map = {}
-            for tk, tv in tensors.items():
-                # up is N, d
-                if tk.endswith("up.weight"):
-                    proc_name = ".".join(tk.split(".")[:-3])
-                    r = tv.shape[1]
-                    name_rank_map[proc_name] = r
+    #         unet_lora_attn_procs = {}
+    #         name_rank_map = {}
+    #         for tk, tv in tensors.items():
+    #             # up is N, d
+    #             if tk.endswith("up.weight"):
+    #                 proc_name = ".".join(tk.split(".")[:-3])
+    #                 r = tv.shape[1]
+    #                 name_rank_map[proc_name] = r
 
-            for name, attn_processor in unet.attn_processors.items():
-                cross_attention_dim = (
-                    None
-                    if name.endswith("attn1.processor")
-                    else unet.config.cross_attention_dim
-                )
-                if name.startswith("mid_block"):
-                    hidden_size = unet.config.block_out_channels[-1]
-                elif name.startswith("up_blocks"):
-                    block_id = int(name[len("up_blocks.")])
-                    hidden_size = list(reversed(unet.config.block_out_channels))[
-                        block_id
-                    ]
-                elif name.startswith("down_blocks"):
-                    block_id = int(name[len("down_blocks.")])
-                    hidden_size = unet.config.block_out_channels[block_id]
-                with no_init_or_tensor():
-                    module = LoRAAttnProcessor2_0(
-                        hidden_size=hidden_size,
-                        cross_attention_dim=cross_attention_dim,
-                        rank=name_rank_map[name],
-                    )
-                unet_lora_attn_procs[name] = module.to("cuda", non_blocking=True)
+    #         for name, attn_processor in unet.attn_processors.items():
+    #             cross_attention_dim = (
+    #                 None
+    #                 if name.endswith("attn1.processor")
+    #                 else unet.config.cross_attention_dim
+    #             )
+    #             if name.startswith("mid_block"):
+    #                 hidden_size = unet.config.block_out_channels[-1]
+    #             elif name.startswith("up_blocks"):
+    #                 block_id = int(name[len("up_blocks.")])
+    #                 hidden_size = list(reversed(unet.config.block_out_channels))[
+    #                     block_id
+    #                 ]
+    #             elif name.startswith("down_blocks"):
+    #                 block_id = int(name[len("down_blocks.")])
+    #                 hidden_size = unet.config.block_out_channels[block_id]
+    #             with no_init_or_tensor():
+    #                 module = LoRAAttnProcessor2_0(
+    #                     hidden_size=hidden_size,
+    #                     cross_attention_dim=cross_attention_dim,
+    #                     rank=name_rank_map[name],
+    #                 )
+    #             unet_lora_attn_procs[name] = module.to("cuda", non_blocking=True)
 
-            unet.set_attn_processor(unet_lora_attn_procs)
-            unet.load_state_dict(tensors, strict=False)
+    #         unet.set_attn_processor(unet_lora_attn_procs)
+    #         unet.load_state_dict(tensors, strict=False)
 
-        # load text
-        handler = TokenEmbeddingsHandler(
-            [pipe.text_encoder, pipe.text_encoder_2], [pipe.tokenizer, pipe.tokenizer_2]
-        )
-        handler.load_embeddings(os.path.join(local_weights_cache, "embeddings.pti"))
+    #     # load text
+    #     handler = TokenEmbeddingsHandler(
+    #         [pipe.text_encoder, pipe.text_encoder_2], [pipe.tokenizer, pipe.tokenizer_2]
+    #     )
+    #     handler.load_embeddings(os.path.join(local_weights_cache, "embeddings.pti"))
 
-        # load params
-        with open(os.path.join(local_weights_cache, "special_params.json"), "r") as f:
-            params = json.load(f)
-        self.token_map = params
+    #     # load params
+    #     with open(os.path.join(local_weights_cache, "special_params.json"), "r") as f:
+    #         params = json.load(f)
+    #     self.token_map = params
 
-        self.tuned_model = True
+    #     self.tuned_model = True
 
     def setup(self, weights: Optional[Path] = None):
         """Load the model into memory to make running multiple predictions efficient"""
@@ -408,6 +408,7 @@ class Predictor(BasePredictor):
         print(f"Scheduler:   {scheduler}")
         print(f"Lora_scale:  {lora_scale}")
         print(f"Style scale: {style_scale}")
+        print(f"Replicate weights: {replicate_weights}")
 
 
         self.load_lora_weights(replicate_weights)
--- train.py
+++ train.py
@@ -8,24 +8,40 @@ from predict import SDXL_MODEL_CACHE, SDXL_URL, download_weights
 OUTPUT_DIR = "training_out"
 
 class TrainingOutput(BaseModel):
-    weights: Path
+    weights_a: Path
+    weights_b: Path
 
 def train(
-        repo_id: str = Input(
-            description="huggingface repo to import ",
+        repo_id_a: str = Input(
+            description="First Hugging Face repo to import",
         ),
-        weights: str = Input(
-            description="safetensor weights to import ",
+        weights_a: str = Input(
+            description="First safetensor weights to import",
         ),
+        repo_id_b: str = Input(
+            description="Second Hugging Face repo to import",
+        ),
+        weights_b: str = Input(
+            description="Second safetensor weights to import",
+        )
 ) -> TrainingOutput:
 
-    dest = 'lora.safetensors'
-    if os.path.exists(dest):
-        os.remove(dest)
-    
-    fn = hf_hub_download(repo_id=repo_id, filename=weights)
+    dest_a = 'lora_a.safetensors'
+    dest_b = 'lora_b.safetensors'
 
-    shutil.copy(fn, dest)
-    os.remove(fn)
-    print(f"weights copied to {dest}")
-    return TrainingOutput(weights=Path(dest))
+    # Download and save first set of weights
+    if os.path.exists(dest_a):
+        os.remove(dest_a)
+    fn_a = hf_hub_download(repo_id=repo_id_a, filename=weights_a)
+    shutil.copy(fn_a, dest_a)
+    os.remove(fn_a)
+
+    # Download and save second set of weights
+    if os.path.exists(dest_b):
+        os.remove(dest_b)
+    fn_b = hf_hub_download(repo_id=repo_id_b, filename=weights_b)
+    shutil.copy(fn_b, dest_b)
+    os.remove(fn_b)
+
+    print(f"Weights copied to {dest_a} and {dest_b}")
+    return TrainingOutput(weights_a=Path(dest_a), weights_b=Path(dest_b))
