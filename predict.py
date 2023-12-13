48cf8bf8984d0f0abe057646d966b573cf57caea
Peter Tountas <petertountas1@gmail.com>
2023-12-13 10:53:49 -0600

    udpates

--- predict.py
+++ predict.py
@@ -183,13 +183,13 @@ class Predictor(BasePredictor):
 
     def setup(self, weights: Optional[Path] = None):
         """Load the model into memory to make running multiple predictions efficient"""
-
+        weights = 'https://replicate.delivery/pbxt/Qip9WYdKPy5eZiZe1AetWnzmYdppkLHMUA8bE6YVOAfZSwGIB/trained_model.tar'
         start = time.time()
         self.tuned_model = False
         self.tuned_weights = None
         self.style_tuned_weights = None
-        if str(weights) == "weights":
-            weights = None
+        # if str(weights) == "weights":
+        #     weights = None
 
         self.weights_cache = WeightsDownloadCache()
 
@@ -223,10 +223,10 @@ class Predictor(BasePredictor):
             for filename in os.listdir("./trained-model"):
                 print('filename: ', filename)
 
-        if weights or os.path.exists("./trained-model"):
+        if weights:
             self.load_trained_weights(weights, self.txt2img_pipe)
 
-        if weights or os.path.exists("./trained-model"):
+        if weights:
             self.load_lora_weights_from_hf(weights, self.txt2img_pipe)
 
         self.txt2img_pipe.to("cuda")
@@ -370,10 +370,10 @@ class Predictor(BasePredictor):
             le=1.0,
             default=0.6,
         ),
-        replicate_weights: str = Input(
-            description="Replicate LoRA weights to use. Leave blank to use the default weights.",
-            default=None,
-        ),
+        # replicate_weights: str = Input(
+        #     description="Replicate LoRA weights to use. Leave blank to use the default weights.",
+        #     default=None,
+        # ),
         disable_safety_checker: bool = Input(
             description="Disable safety checker for generated images. This feature is only available through the API. See [https://replicate.com/docs/how-does-replicate-work#safety](https://replicate.com/docs/how-does-replicate-work#safety)",
             default=False
@@ -384,8 +384,8 @@ class Predictor(BasePredictor):
             seed = int.from_bytes(os.urandom(2), "big")
         print(f"Using seed: {seed}")
 
-        if replicate_weights:
-            self.load_trained_weights(replicate_weights, self.txt2img_pipe)
+        # if replicate_weights:
+            # self.load_trained_weights(replicate_weights, self.txt2img_pipe)
         
         # OOMs can leave vae in bad state
         if self.txt2img_pipe.vae.dtype == torch.float32:
